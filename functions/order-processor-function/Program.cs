a1077ffaf742d98f2bb55d6f629dd1b2b861e6e7
Chetan Chandrapal <chandrapal13.cc@gmail.com>
2026-04-24 01:18:12 +0530

    Integrated Function with Payment and Inventory services

--- functions/order-processor-function/Program.cs
+++ functions/order-processor-function/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Hosting;
 var builder = FunctionsApplication.CreateBuilder(args);
 
 builder.ConfigureFunctionsWebApplication();
+builder.Services.AddHttpClient();
 
 builder.Services
     .AddApplicationInsightsTelemetryWorkerService()
---
+++ inventory-service/inventory-service/Controllers/InventoryController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace inventory_service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InventoryController : ControllerBase
+    {
+
+        [HttpPost]
+        public IActionResult Update()
+        {
+            return Ok("Inventory updated");
+        }
+    }
+}
