04e261a6106069e5446ddf2457fe5947bf62db3e
Chetan Chandrapal <chandrapal13.cc@gmail.com>
2026-05-22 13:57:56 +0530

    Implement-AUTH service API project

---
+++ auth-service/auth-service/Controllers/AuthController.cs
@@ -0,0 +1,47 @@
+﻿using auth_service.Data;
+using auth_service.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace auth_service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly AuthDbContext _context;
+        private readonly TokenService _tokenService;
+
+        public AuthController(AuthDbContext context, TokenService tokenService)
+        {
+            _context = context;
+            _tokenService = tokenService;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginRequest request)
+        {
+            var user = await _context.Users
+                .FirstOrDefaultAsync(x =>
+                    x.Username == request.Username &&
+                    x.Password == request.Password);
+
+            if (user == null)
+                return Unauthorized();
+
+            var token = _tokenService.CreateToken(
+                user.Username,
+                user.Role);
+
+            return Ok(token);
+        }
+    }
+
+    public class LoginRequest
+    {
+        public string Username { get; set; } = string.Empty;
+
+        public string Password { get; set; } = string.Empty;
+    }
+}
